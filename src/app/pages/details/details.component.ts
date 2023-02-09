02f010ef7da3059bd7fa84eb1b70e8fd7cd7b6d3
Pango <Spangani37@gmail.com>
2023-02-09 17:02:14 +0400

    first commit

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -12,18 +12,9 @@ export class DetailsComponent {
   // countries:any [];
 
   apiCountry:any = {};
-  countryName:any;
+  countryName:string;
   constructor(private route:ActivatedRoute,private _api:countriesService){
-  //   route.queryParams.forEach((param) =>{
-  //     this.queryParams = param;
-  //     console.log(this.queryParams.name)
-  //   })
-
-  //   this.chosenCountry = this.countries.find((x:any) => {
-  //    return x.countryName == this.queryParams.name;
-  //   })
     
-  //   console.log(this.chosenCountry)
   this.countryName = route.snapshot.params['name'];
 
     this._api.getCountriesAPI().subscribe((response:any) =>{
@@ -32,38 +23,6 @@ export class DetailsComponent {
       })
       console.log(this.apiCountry)
     })
-
-    // this.countries = this._api.getCountries()
-    // setTimeout(() => {
-    //   console.log(this.apiCountry);
-    // }, 2000);
-
-  //   this.countries = this._api.getCountries();
-  
-  // this.chosenCountry = this.apiCountry.find((country:any) =>{
-  //   return country.name.common = this.countryName;
-  // })
-  // this.chosenCountry = this.apiCountry.find((country:any)=>{
-  //   return country.name.common = this.countryName;
-  // })
-  // console.log(this.chosenCountry)
   }
 }
 
-// private queryParams: any;
-
-// chosenProduct:any = {};
-
-// constructor(private route:ActivatedRoute){
-  
-//   route.queryParams.forEach(param =>{
-//     this.queryParams = param;
-//   })
-    
-//   this.chosenProduct = this.products.find((product) => {
-//    return product.productId == this.queryParams.id;
-//   })
-  
-//   console.log(this.chosenProduct)
-  
-// }
